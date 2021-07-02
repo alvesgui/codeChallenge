e5b71c4279bf9c4a8bb187cc2a974eaadbfc85e2
Guilherme <guylherm.alves14@gmail.com>
2021-07-02 19:02:18 -0300

    finzalizando api

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 import express from "express";
-import "express-async-erros";
+import "express-async-errors";
+import cors from "cors";
+
 import path from "path";
 import errorHandler from "./errors/handle";
 
@@ -8,6 +10,7 @@ import routes from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
